314c32b1e275fef5232a155789720ce882c2361a
yashwanth.varakala <y.varakala@phytec.de>
2022-03-21 16:25:00 +0100

    python_v1

---
+++ gittest.py
@@ -0,0 +1,10 @@
+start= 'home'
+dest= 'dornsheimer weg'
+travel = 80 #time in minutes
+time_h = int(input('How much time it took for walking from home to stop'))
+walking_from_home = time_h
+
+walking_from_dornsheimer=13
+
+Total= walking_from_home + travel + walking_from_dornsheimer
+print(Total)
